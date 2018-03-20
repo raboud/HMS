abb9c2b273ac6da934e59afd7c2c7ce4f02c4bcd
Robert Raboud <robert.raboud@randreng.com>
2018-03-20 13:01:12 -0400

    resource cleanup and more font awesome

--- HmsWeb/src/app/store/catalog/catalog.module.ts
+++ HmsWeb/src/app/store/catalog/catalog.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -17,6 +18,7 @@ import { CatalogComponent } from './catalog.component';
     imports: [
       BrowserModule,
       SharedModule,
+      FontAwesomeModule,
       NgbModule,
         ],
     declarations: [
